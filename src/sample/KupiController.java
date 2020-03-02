7f80f2650edceb294a58141e2239ce0d3ef06b18
Faris135 <48414463+Faris135@users.noreply.github.com>
2020-03-02 12:30:42 +0100

    Kupi.fxml i KupiController

---
+++ src/sample/KupiController.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+
+public class KupiController {
+
+
+    public Spinner spinnerKupi;
+    public Button odustaniBtn;
+    public Button kupiBtn;
+}
